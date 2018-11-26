d66a1a18de1e30fc967af20e5645dd4c76934ebc
MrLittlewell <40912379+MrLittlewell@users.noreply.github.com>
2018-11-26 21:12:14 +0300

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const controller = require('../controller/auth')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+	
+})
+
+module.exports = router
+
+//localhost:5000/api/auth/login
