3b3e21f6bf54154c3335a6c023763ad058b6595d
MrLittlewell <40912379+MrLittlewell@users.noreply.github.com>
2018-12-03 23:37:01 +0300

    23.36

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,37 @@
-const bcrypt = require('bcryptjs')
-const User = require('../models/User')
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const User = require("../models/User")
+const keys = require("../config/keys")
 
 
-module.exports.login = function(req, res) {
-	res.status(200).json({
-			login: {
-			email: req.body.email,
-			password: req.body.password
-			}
-		})	
+module.exports.login = async function(req, res) {
+	const candidate = await User.findOne({email: req.body.email})
+
+	if (candidate) {
+		//проверка пароля
+		const passwordResult = bcrypt.compareSycn(req.body.password, candidate.password)
+		if (passwordResult) {
+			//Генерация токина, пароли совпали
+			const token = jwt.sign({
+				email: candidate.email,
+				userId: candidate._id
+			}, keys.jwt, {expiresIn: 60 * 60})
+
+			res.status(200).json({
+				token: token
+			})
+		} else {
+			//Пароли не совпали
+			res.status(401).json({
+				message: 'Пароли не совпадвают.'
+			})
+		}
+	} else {
+		//пользователя нет. ошибка
+		res.status(404).json({
+			message: 'пользователь с таким email не найден.'
+		})
+	}
 }
 
 module.exports.register = async function(req, res) {
@@ -37,3 +60,5 @@ module.exports.register = async function(req, res) {
 		
 	}
 }
+
+//НАЙТИ ОШИБКУ. ПОСМОТРЕТЬ ПРОШЛЫЙ УРОК
