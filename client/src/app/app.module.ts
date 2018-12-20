89fc836c044f2920f064e5690b842d55d9f5ce57
MrLittlewell <40912379+MrLittlewell@users.noreply.github.com>
2018-12-20 23:53:39 +0300

    20.12

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
-import { FormsModule, ReactiveFormsModule} from '@angular/forms'
-import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
 
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { LoginPageComponent } from './login-page/login-page.component'
-import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component'
-import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component'
-import { RegisterPageComponent } from './register-page/register-page.component'
-import { TokenInterceptor} from './shared/classes/token.interceptor';
-import { OverviewPageComponent } from './overview-page/overview-page.component'
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {LoginPageComponent} from './login-page/login-page.component'
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component'
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component'
+import {RegisterPageComponent} from './register-page/register-page.component'
+import {TokenInterceptor} from './shared/classes/token.interceptor'
+import {OverviewPageComponent} from './overview-page/overview-page.component'
+import {AnalyticsPageComponent} from './analytics-page/analytics-page.component'
+import {HistoryPageComponent} from './history-page/history-page.component'
+import {OrderPageComponent} from './order-page/order-page.component'
+import {CategoriesPageComponent} from './categories-page/categories-page.component'
+import {LoaderComponent} from './shared/components/loader/loader.component';
+import { CategoriesFormComponent } from './categories-page/categories-form/categories-form.component';
+import { PositionsFormComponent } from './categories-page/categories-form/positions-form/positions-form.component'
 
 
 @NgModule({
@@ -21,7 +28,14 @@ import { OverviewPageComponent } from './overview-page/overview-page.component'
     AuthLayoutComponent,
     SiteLayoutComponent,
     RegisterPageComponent,
-    OverviewPageComponent
+    OverviewPageComponent,
+    AnalyticsPageComponent,
+    HistoryPageComponent,
+    OrderPageComponent,
+    CategoriesPageComponent,
+    LoaderComponent,
+    CategoriesFormComponent,
+    PositionsFormComponent
   ],
   imports: [
     BrowserModule,
@@ -39,4 +53,5 @@ import { OverviewPageComponent } from './overview-page/overview-page.component'
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- client/src/app/shared/classes/token.interceptor.ts
+++ client/src/app/shared/classes/token.interceptor.ts
@@ -1,40 +1,40 @@
 import {Injectable} from '@angular/core';
 import {AuthService} from '../services/auth.service';
-import {HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest, HttpEvent} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 import {Router} from '@angular/router';
-import {error} from "util";
+
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 	constructor(private auth: AuthService, private router: Router) {
 	}
 
-	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
-		if (this.auth.isAuthenticated()) {
-			req = req.clone({
-				setHeaders: {
-					Authorization: this.auth.getToken()
-				}
-			});
-		}
-		return next.handle(req).pipe(
-			catchError(
-				(error: HttpErrorResponse) => this.handleAuthError(error)
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+				if (this.auth.isAuthenticated()) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: this.auth.getToken()
+        }
+      })
+    }
+    return next.handle(req).pipe(
+      catchError(
+        (error: HttpErrorResponse) => this.handleAuthError(error)
 				)
-			);
+			)
 	}
 
-	private handleAuthError(HttpErrorResponse): Observable<any> {
+	private handleAuthError(error: HttpErrorResponse): Observable<any> {
 		if (error.status === 401) {
 			this.router.navigate(['/login'], {
 				queryParams: {
 					sessionFailed: true
 				}
-			});
+			})
 		}
 
-		return throwError(error);
+		return throwError(error)
 	}
 }
